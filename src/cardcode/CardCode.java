f389735839e4cf56603162cd41ac93381906184f
szablowm <szablowm@sheridancollege.ca>
2021-02-13 13:33:41 -0500

    Matthew Szablowski 991619660

--- src/cardcode/CardCode.java
+++ src/cardcode/CardCode.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-//package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
+
 
 /**
  *
- * @author Ronak
+ * @author Matthew Szablowski 991619660
  */
 public class CardCode {
 
--- src/cardcode/CardTrick.java
+++ src/cardcode/CardTrick.java
@@ -1,14 +1,17 @@
 
-//package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
-
 /**
  *
- * @author Ronak
+ * @author Matthew Szablowski 991619660
+ * 
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-      
+       CardCode card = new CardCode();
+       
+       card.setSuit("Clubs");
+       card.setValue(6);
+       System.out.println(card.getSuit() + " " + card.getValue());
     }
     
 }
